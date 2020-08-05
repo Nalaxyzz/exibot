2cd43750824b095fbd2ac8cf7e0a22ddbd88ceb6
exicraft <69243683+exicraft@users.noreply.github.com>
2020-08-05 21:18:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const    client = new Discord.Client();
 
 
 
-var prefix = "="
+var prefix ="=/"
 
 
 client.login("NzI5ODIyMjIwNDczMzM1ODY5.XwSBPQ.gJopaJwbiXbT7uHSchC0Jyrnhak")
 
 client.on("ready", () =>{
     console.log("je suis prêt")
-    client.user.setGame("Go to the space")
+    client.user.setGame("/help our l'aide !")
 });
 
  client.on('message', message => { 
